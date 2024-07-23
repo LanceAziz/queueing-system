a86a53c2cc824cd61f9a9008affda20a6da9b65d
lance aziz <lanceaziz2002@gmail.com>
2024-07-23 23:02:48 +0300

    project restructure

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+import "./globals.css";
+import localFont from '@next/font/local'
+
+export const metadata = {
+  title: "المعهد العالي للدراسات المتطورة",
+  description: "المعهد العالي للدراسات المتطورة",
+};
+
+const FrutigerLT = localFont({
+  src: [
+    {
+      path: '../../public/Fonts/FrutigerLTArabic-45Light.ttf',
+      style: 'light'
+    },
+    {
+      path: '../../public/Fonts/FrutigerLTArabic-55Roman.ttf',
+      style: 'medium'
+    },
+    {
+      path: '../../public/Fonts/FrutigerLTArabic-65Bold.ttf',
+      style: 'bold'
+    }
+  ],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar" dir="rtl" className={FrutigerLT.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,34 @@
+"use client"
+import Image from "next/image";
+import { useEffect } from 'react';
+import logo from "../../public/Images/Logo.png"
+
+export default function Home() {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'Enter') {
+        nextUser();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+  const nextUser = () => {
+    console.log('nest user is on his way to your');
+  }
+  return (
+    <main className="h-screen bg-light flex flex-col justify-center items-center text-dark">
+      <div className="w-20">
+        <Image src={logo} alt="المعهد العالي للدراسات" />
+      </div>
+      <h1 className="text-xl text-dark">المعهد العالي للدراسات المتطورة</h1>
+      <h2 className="pb-4 text-lg text-dark">اهلاً بك، مستخدم <span>1</span></h2>
+      <div className="bg-semi-light text-dark rounded-3xl py-5 px-8 flex flex-col text-xl">
+        <div className="pb-5 text-center">عميل رقم: <span>301</span></div>
+        <button onClick={nextUser} className="bg-light hover:bg-primary hover:text-light transform duration-300 text-semi-dark px-6 py-3 rounded-full text-center">العميل التالي</button>
+      </div>
+    </main>
+  );
+}
